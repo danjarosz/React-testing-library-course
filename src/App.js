ee9e7b477315ac36be1b61f997fecacb88de9ff4
Daniel Jarosz <jarosz.daniel@protonmail.com>
2022-01-25 18:43:40 +0100

    tests(App): inputs should be initially empty

--- src/App.js
+++ src/App.js
@@ -1,8 +1,43 @@
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div>
+    <div className="container my-5">
+      <form>
+        <div className="mp-3">
+          <label htmlFor="email" className="form-label">
+            Email address
+          </label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            className="form-control"
+          />
+        </div>
+        <div className="mp-3">
+          <label htmlFor="password" className="form-label">
+            Password
+          </label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            className="form-control"
+          />
+        </div>
+        <div className="mp-3">
+          <label htmlFor="confirm-password" className="form-label">
+            Confirm password
+          </label>
+          <input
+            type="password"
+            id="confirm-password"
+            name="confirm-password"
+            className="form-control"
+          />
+        </div>
+      </form>
     </div>
   );
 }
