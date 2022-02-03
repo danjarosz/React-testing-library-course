04eaa671af4d73a53500d8015c618de3c2c09c26
Daniel Jarosz <jarosz.daniel@protonmail.com>
2022-02-03 19:31:47 +0100

    tests(Pets): using Mock Service Worker

--- app-2/client/src/components/Pets/__test__/Pets.test.js
+++ app-2/client/src/components/Pets/__test__/Pets.test.js
@@ -1,5 +1,33 @@
 import { screen, render } from "@testing-library/react";
+import { rest } from "msw";
+import { setupServer } from "msw/node";
+import Pets from "../Pets";
+import catsMock from "../../../mocks/cats.json"
+
+const server = setupServer(
+    rest.get('http://localhost:4000/cats', (req, res, ctx) => {
+      return res(
+          ctx.status(200),
+          ctx.json(catsMock)
+      )
+    }),
+)
+
+
+beforeAll(() => {
+  server.listen();
+})
+afterEach(() => {
+  server.resetHandlers();
+})
+afterAll(() => {
+  server.close();
+})
 
 describe("Pets", () => {
-  it("should", () => {});
+  it("should render the correct amount of cats", async () => {
+    render(<Pets />);
+    const cards = await screen.findAllByRole("article");
+    expect(cards.length).toBe(5);
+  });
 });
